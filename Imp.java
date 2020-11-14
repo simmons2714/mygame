0546da454f4f234479447cd0cdd31542efea4f29
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2020-11-14 16:12:03 -0500

    Update Imp.java

--- Imp.java
+++ Imp.java
@@ -1,9 +1,6 @@
-public class Imp extends Monster{
-    public void UnImp(){
-        setAtk(6);
-        setName("imp");
-        setDex(0.07);
-        setHp(25);
-        setFlavorText("a small imp its red");
+public class Imp extends NPC {
+    public Imp(){
+        super(25, 0.07, 6, "imp", "a small creature and its red");
     }
+
 }
