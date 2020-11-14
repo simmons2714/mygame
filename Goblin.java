ed928bf2234c24353303a0252b23df729fcee297
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2020-11-14 09:14:15 -0500

    Create Goblin.java

---
+++ Goblin.java
@@ -0,0 +1,9 @@
+public class Goblin extends Monster{
+    public void UnGoblin(){
+        setAtk(3);
+        setName("goblin");
+        setDex(0.05);
+        setHp(20);
+        setFlavorText("a small creature its green");
+    }
+}
