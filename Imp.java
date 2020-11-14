f5c47b4ed003d8b8c8c47e604fc176a225d7e716
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2020-11-14 09:14:42 -0500

    Create Imp.java

---
+++ Imp.java
@@ -0,0 +1,9 @@
+public class Imp extends Monster{
+    public void UnImp(){
+        setAtk(6);
+        setName("imp");
+        setDex(0.07);
+        setHp(25);
+        setFlavorText("a small imp its red");
+    }
+}
