74e49ad9d2fe25f20515445452c33c258852c642
simmons2714 <67743604+simmons2714@users.noreply.github.com>
2020-11-14 16:10:29 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,12 +1,19 @@
+import java.util.*;
+
 public class Game{
 
     public static void main(String[] args) {
+
         Goblin testGoblin = new Goblin();
         Imp testImp = new Imp();
-        testImp.UnImp();
-        testGoblin.UnGoblin();
+        Melon testMelon = new Melon();
 
-        System.out.println("The " +  testImp.getName() + " has an attack of " + testImp.getAtk() + ". The imp HP is " + testImp.getHp() );
-        System.out.println("The " +  testGoblin.getName() + " has an attack of " + testGoblin.getAtk() + ". The goblin HP is " + testGoblin.getHp() );
+        System.out.println("The " + testImp.getName() + " has an attack of " + testImp.getAtk() + ". The imp HP is " + testImp.getHp());
+        System.out.println("The " + testGoblin.getName() + " has an attack of " + testGoblin.getAtk() + ". The goblin HP is " + testGoblin.getHp());
+
+        while(testGoblin.isDead() ^ testImp.isDead()) {
+            System.out.println(testGoblin.inCombat(testImp));
+        }
+
     }
 }
